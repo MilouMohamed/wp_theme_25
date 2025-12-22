c29efe4bb3f182f0891726c48821a1a07549a2a4
Milou Mohamed MilouMohamed <49353852+MilouMohamed@users.noreply.github.com>
2025-12-22 23:39:33 +0100

    Add files via upload

---
+++ medoxMil/404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<div class="container not-fond-page text-center">
+
+    <img src="<?php echo  get_template_directory_uri() . "/images/404.webp" ?>" alt="Image Not Fond 404">
+
+  <h1>Page non trouvée (404)</h1>
+<p class="descrip">Désolé, cette page n’existe pas medox. wwww</p>
+
+</div>
+<?php get_footer(); ?>
